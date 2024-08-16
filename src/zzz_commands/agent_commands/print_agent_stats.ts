26422772460666b0457e42932ebf5dde0579c9e2
keilo <keilogamimg@gmail.com>
2024-08-16 02:25:12 -0300

    moving base agent embed somewhere else

--- src/zzz_commands/agent_commands/print_agent_stats.ts
+++ src/zzz_commands/agent_commands/print_agent_stats.ts
@@ -1,37 +1,6 @@
 import {Agent} from "../../model/Agent";
 import {Emote} from "../../enums/emote";
-
-function generateBaseAgentEmbed(agent: Agent) {
-    const agentEmbed: { [k: string]: any } = {
-        title: agent.fullName ? agent.fullName : agent.name,
-        description:
-            `${agent.faction.emote.emote} • ${agent.faction.faction}\n` +
-            `${agent.rarity.agentEmote.emote} ${agent.element.emote.emote} ` +
-            `${agent.specialty.emote.emote} ${agent.damageType.emote.emote}`,
-        thumbnail: {
-            url: agent.iconImageUrl,
-        },
-        color: agent.embedColor,
-        fields: [],
-    }
-
-    if (agent.releasePatch == null)
-        agentEmbed.footer = {
-            text:
-                "This entry is from skeleton data left on the game by MiHoYo, it doesn't mean " +
-                "this is releasing soon, or at all, and all information is subject to changes before release."
-        }
-
-    else if (agent.releasePatch > 2)
-        agentEmbed.footer = {
-            text:
-                "This entry is from a future version, all information on it could be " +
-                "inaccurate and is subject to changes before release."
-        }
-
-    return agentEmbed;
-}
-
+import {generateBaseAgentEmbed} from "./generate_base_agent_embed";
 
 export function printAgentStats(agent: Agent): any {
     const agentEmbed = generateBaseAgentEmbed(agent);

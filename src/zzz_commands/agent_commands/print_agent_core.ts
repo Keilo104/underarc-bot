3edcc8786bd5f5335ac04d2237cd86410cf0cfa2
keilo <keilogamimg@gmail.com>
2024-08-16 05:40:50 -0300

    starting some work on core skill

--- src/zzz_commands/agent_commands/print_agent_core.ts
+++ src/zzz_commands/agent_commands/print_agent_core.ts
@@ -12,11 +12,11 @@ export function printAgentCore(agent: Agent) {
     });
 
     agentEmbed.fields.unshift({
-        name: "Core Stat", inline: false,
+        name: "Core Stat Lv0 → LvF", inline: false,
         value:
             `${agent.firstCoreStat.emote.emote} **${agent.firstCoreStat.displayName}:** ` +
             `${agent.firstCoreStatAtLevel(1)} → ${agent.firstCoreStatAtLevel(60)}`
-    })
+    });
 
     for (let i = agent.coreSkillNames.length - 1 ; i > -1 ; i--) {
         agentEmbed.fields.unshift({
@@ -30,13 +30,26 @@ export function printAgentCore(agent: Agent) {
 
 export function printAgentCoreAtLevel(agent: Agent, level: number) {
     const agentEmbed = generateBaseAgentEmbed(agent);
+    let coreLevels = [0, "A", "B", "C", "D", "E", "F"];
 
     agentEmbed.fields.unshift({
         name: "Core Skill Upgrade Materials", inline: false,
         value:
             `${agent.purpleCoreMat.emote.emote} ${agent.purpleCoreMat.purpleCoreMat}\n` +
             `${agent.goldenCoreMat.emote.emote} ${agent.goldenCoreMat.goldenCoreMat}`
-    })
+    });
+
+    agentEmbed.fields.unshift({
+        name: "this isn't working btw", inline: false,
+        value: "i'm working on it, i'm working on it"
+    });
+
+    agentEmbed.fields.unshift({
+        name: `Core Stat at Lv${coreLevels[level]}`, inline: false,
+        value:
+            `${agent.firstCoreStat.emote.emote} **${agent.firstCoreStat.displayName}:** ` +
+            `${agent.firstCoreStatAtLevel(level)}`
+    });
 
     return agentEmbed;
 }

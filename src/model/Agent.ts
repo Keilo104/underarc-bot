33fc806326f396cc88fcd97cdb65118a36901b1d
keilo <keilogamimg@gmail.com>
2024-08-16 04:30:40 -0300

    adding crit dmg to treatstring

--- src/model/Agent.ts
+++ src/model/Agent.ts
@@ -329,6 +329,7 @@ export class Agent {
             .replaceAll("Anomaly Mastery", `${Emote.ANOMALY_MASTERY_STAT_ICON.emote}Anomaly Mastery`)
             .replaceAll("Anomaly Proficiency", `${Emote.ANOMALY_PROFICIENCY_STAT_ICON.emote}Anomaly Proficiency`)
             .replaceAll("CRIT Rate", `${Emote.CRIT_RATE_STAT_ICON.emote}CRIT Rate`)
+            .replaceAll("CRIT DMG", `${Emote.CRIT_DMG_STAT_ICON.emote}CRIT DMG`)
             .replaceAll("<color=#2eb6ff>", `${Emote.ELECTRIC_ICON.emote}`)
             .replaceAll("<color=#fe437e>", `${Emote.ETHER_ICON.emote}`)
             .replaceAll("<color=#f0d12b>", `${Emote.PHYSICAL_ICON.emote}`)

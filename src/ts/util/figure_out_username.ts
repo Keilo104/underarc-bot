62007f3a3495afc930eb86d673ff1598e4636e97
keilo <keilogamimg@gmail.com>
2025-03-04 18:09:21 -0300

    fixing FigureOutUsername logic to use server nicknames properly

--- src/ts/util/figure_out_username.ts
+++ src/ts/util/figure_out_username.ts
@@ -4,8 +4,8 @@ export function FigureOutUsername(interaction: any): string {
     if(interaction.context == InteractionContextType.PRIVATE_CHANNEL)
         return interaction.user.global_name;
 
-    else if (interaction.nick != null)
-        return interaction.nick;
+    else if (interaction.member.nick != null)
+        return interaction.member.nick;
 
     return interaction.member.user.global_name;
 }
